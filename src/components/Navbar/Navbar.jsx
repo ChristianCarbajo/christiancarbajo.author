39709a9a5df52aef933c547e2baa8602d41c7294
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-07-28 03:30:09 +0200

    added stories

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom"
 const Navbar = () => {
     return (
         <div className='navBar'>
+            
             <Link to="/about"><p>¿Quién soy?</p></Link>
             <Link to="/"><p>Libros publicados</p></Link>
             <Link to="/"><p>Escribiendo</p></Link>
